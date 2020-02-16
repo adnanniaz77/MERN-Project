b267458eed6ad3ed06d67c6537f5d14f1e11db33
Adnan Niaz <adnanniaz77@yahoo.com>
2020-02-16 07:33:54 -0800

    successfully integrated JWT tokens

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
-const { check, validationResult } = require("express-validator/check");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
 
 // @route   POST api/users
 // @desc    Register user
@@ -65,9 +67,21 @@ router.post(
 
             await user.save();
 
-            // Return jsonwebtoken
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
 
-            res.send("User Registered");
+            jwt.sign(
+                payload,
+                config.get("jwtSecret"),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
         } catch (err) {
             console.error(err.message);
             res.status(500).send("Server Error");
